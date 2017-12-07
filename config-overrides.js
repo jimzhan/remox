c60faeefe21568e1c9fac8470de8cc0edfa5450b
Jim Zhan <jim.zhan@me.com>
2017-12-07 10:29:25 +0800

    added decorators & class properties supports for both CLI & VSCode

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,11 @@
+const { injectBabelPlugin } = require('react-app-rewired')
 const rewireEslint = require('react-app-rewire-eslint')
 
 module.exports = function override (config, env) {
+  config = injectBabelPlugin([
+    'transform-decorators-legacy',
+    'transform-class-properties'
+  ], config)
   config = rewireEslint(config, env)
   return config
 }
